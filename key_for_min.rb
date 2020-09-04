ad1dc75eeffd60cf9d7121b2ea5175df970ba891
Jacqueline <lojacqueline2@gmail.com>
2020-09-04 15:35:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  key_for_min_value.each do |kid, age|
+    puts "Happy Birthday #{kid}! You are now #{age} years old!"
+  end
 end
