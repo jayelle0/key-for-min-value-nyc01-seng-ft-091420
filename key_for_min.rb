7fa4bf639cfae451cc90f0da96b9fad840fc2aa3
Jacqueline <lojacqueline2@gmail.com>
2020-09-04 15:52:44 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,10 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  key_for_min_value.each do |key, value|
+  key_for_min_value.each do |item, price|
     greatest  = ""
-    if value > greatest  
-      puts key 
+    if price > greatest  
+      puts item 
     end
   end
 end
