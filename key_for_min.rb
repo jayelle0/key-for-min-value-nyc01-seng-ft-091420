4bad0100f949391cc3bf8c411004f131c4670790
Jacqueline <lojacqueline2@gmail.com>
2020-09-04 17:04:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,12 +1,14 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
+require 'pry'
 def key_for_min_value(name_hash)
-  lowest_key = nil
+   binding.pry
+  # lowest_key = name_hash[value]
 name_hash.each do |key,value|
    puts value
-   if value < lowest_key[value]
+   if value < name_hash[lowest_key]
      lowest_key = key
   end
-  
+
 end
